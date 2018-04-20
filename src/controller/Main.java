4cf9901f183fd3da2ed217cab23518d52aee692c
getileino <getileino@gmail.com>
2018-04-20 11:11:05 +0200

    Versión 1.3. (K-medias) Pruebas de repositorio

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,4 @@
-/*/////
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +13,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 /**
  *
  * @author Adrián-Trabajo
